23f6c5dce7186b1bd7919568410b4a49652fdc24
Matthew Wester <MatthewWester@live.com>
2020-03-05 15:00:39 -0700

    Tweak sequelize consts to get deployment to heroku

--- models/index.js
+++ models/index.js
@@ -2,11 +2,12 @@
 
 const fs = require("fs");
 const path = require("path");
-const sequelize = require("sequelize");
+const Sequelize = require("sequelize");
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
+let sequelize;
 
 if (config.use_env_variable) {
   let sequelize = new Sequelize(process.env[config.use_env_variable]);
