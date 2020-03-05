a345b58bd210c115a11837b24a6287dee75f596c
Matthew Wester <MatthewWester@live.com>
2020-03-05 15:08:16 -0700

    do not need to use let again when declared earlier

--- models/index.js
+++ models/index.js
@@ -10,9 +10,9 @@ const db = {};
 let sequelize;
 
 if (config.use_env_variable) {
-  let sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  let sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
