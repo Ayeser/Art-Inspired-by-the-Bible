52a2154427c3d97332a0d1388f93fc2db59d4e89
Matthew Wester <MatthewWester@live.com>
2020-03-05 17:38:24 -0700

    trying to identify db because server now working but path unidentified

--- controllers/bible_controller.js
+++ controllers/bible_controller.js
@@ -6,11 +6,13 @@
 
 // The following section is just to check the artpiece database
 const db = require("../models/artPieces.js");
+const path = require("path");
 
 module.exports = function(app) {
 
     // GET route for getting all of the todos
     app.get("/api/genesisArt", function(req, res) {
+      console.log(db);
       // findAll returns all entries for a table when used with no options
       db.genesisArt.findAll({}).then(function(dbgenesisArt) {
         // We have access to the todos as an argument inside of the callback function
