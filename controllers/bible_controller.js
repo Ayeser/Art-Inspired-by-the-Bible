64c3417b28dd926d37588dbb7fe573f267133958
Matthew Wester <MatthewWester@live.com>
2020-03-05 17:29:36 -0700

    Server is now working so uncommenting other lines

--- controllers/bible_controller.js
+++ controllers/bible_controller.js
@@ -10,18 +10,18 @@ const db = require("../models/artPieces.js");
 module.exports = function(app) {
 
     // GET route for getting all of the todos
-    // app.get("/api/genesisArt", function(req, res) {
-    //   // findAll returns all entries for a table when used with no options
-    //   db.genesisArt.findAll({}).then(function(dbgenesisArt) {
-    //     // We have access to the todos as an argument inside of the callback function
-    //     res.json(dbgenesisArt);
-    //   });
+    app.get("/api/genesisArt", function(req, res) {
+      // findAll returns all entries for a table when used with no options
+      db.genesisArt.findAll({}).then(function(dbgenesisArt) {
+        // We have access to the todos as an argument inside of the callback function
+        res.json(dbgenesisArt);
+      });
   
-    // });
+    });
 
     app.get("/", function(req, res) {
-      // res.sendFile(path.join(__dirname, "../public/practicePage.html"));
-      res.status(200).json("testing");
+      res.sendFile(path.join(__dirname, "../public/practicePage.html"));
+      // res.status(200).json("testing");
     });
   
   };
