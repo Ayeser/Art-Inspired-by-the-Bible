26fb4a2d8b90eda68571b127391c69154a292df5
David Foster <foster_db@yahoo.com>
2020-03-05 20:43:07 -0700

    added comments to explain code

--- controllers/bible_controller.js
+++ controllers/bible_controller.js
@@ -10,17 +10,20 @@ const path = require("path");
 
 module.exports = function(app) {
 
+// route for landing page
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/practicePage.html"));
     // res.status(200).json("testing");
   });
 
+// route for all images in Artwork table
   app.get("/api/Artwork", function(req, res) {
     db.Artwork.findAll({}).then(function(dbArtwork) {
       res.json(dbArtwork);
     });
   });
 
+// route joining Artwork table and Scriptures(?) table
   app.get("/api/Artwork/:book", function(req, res) {
     db.Artwork.findBook({
       where: {
