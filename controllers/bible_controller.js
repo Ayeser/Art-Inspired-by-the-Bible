fe1e200a94f9a217fd42a8be4e917c3109230493
David Foster <foster_db@yahoo.com>
2020-03-05 20:40:10 -0700

    creating routes

--- controllers/bible_controller.js
+++ controllers/bible_controller.js
@@ -10,21 +10,25 @@ const path = require("path");
 
 module.exports = function(app) {
 
-    // GET route for getting all of the todos
-    app.get("/api/genesisArt", function(req, res) {
-      console.log(db);
-      // findAll returns all entries for a table when used with no options
-      db.genesisArt.findAll({}).then(function(dbgenesisArt) {
-        // We have access to the todos as an argument inside of the callback function
-        res.json(dbgenesisArt);
-      });
-  
-    });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/practicePage.html"));
+    // res.status(200).json("testing");
+  });
 
-    app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/practicePage.html"));
-      // res.status(200).json("testing");
+  app.get("/api/Artwork", function(req, res) {
+    db.Artwork.findAll({}).then(function(dbArtwork) {
+      res.json(dbArtwork);
     });
-  
-  };
-  
+  });
+
+  app.get("/api/Artwork/:book", function(req, res) {
+    db.Artwork.findBook({
+      where: {
+        id: req.params.book
+      },
+      // include: []
+    }).then(function(dbArtwork) {
+      res.json(dbArtwork);
+    });
+  });
+};
