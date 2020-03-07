7f5de93a1b9df75fafefe06db37824d9ef7e1790
Matthew Wester <MatthewWester@live.com>
2020-03-07 00:50:19 -0700

    added back event.preventDefault in frontendScripts

--- public/assets/js/frontendScripts.js
+++ public/assets/js/frontendScripts.js
@@ -1,18 +1,16 @@
-$(document).ready(function() {
-    $("#searchButton").click(function(){
+$(document).ready(function () {
+    $("#searchButton").click(function () {
+        event.preventDefault();
         console.log("searchArtwork function starts");
-    var searchTerm = $("#searchArtwork").val().toLowerCase();
+        var searchTerm = $("#searchArtwork").val().toLowerCase();
         var firstWord = codeLine.substr(0, codeLine.indexOf(" "));
-    console.log("The first word of search is " + firstWord);
+        console.log("The first word of search is " + firstWord);
+        $.ajax({
+            url: "https://https://glacial-falls-40496.herokuapp.com/api/Artwork/:" + searchTerm,
+            method: "GET"
+        }).then(function (response) {
+            $("#resultsHere").empty();
+            $("#resultsHere").append(JSON.stringify(response));
+        })
     })
-
 });
-
-    // $.ajax({
-    //     url: "https://https://glacial-falls-40496.herokuapp.com/api/Artwork/:" + searchTerm,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     $("#resultsHere").empty();
-    //     $("#resultsHere").append(JSON.stringify(response));
-    // })
-// });
