4fb829f1952ed4c3f2d0e0715f4a9963e94faec5
Matthew Wester <MatthewWester@live.com>
2020-03-06 22:49:33 -0700

    confirmed relative paths on html page working

--- public/assets/js/frontendScripts.js
+++ public/assets/js/frontendScripts.js
@@ -2,13 +2,14 @@ $("#searchForm").submit(function(event) {
     event.preventDefault();
     console.log("searchArtwork function starts");
     var searchTerm = $("#searchArtwork").val().toLowerCase();
-    var firstWord = codeLine.substr(0, codeLine.indexOf(" "));
-    console.log(firstWord);
-    $.ajax({
-        url: "https://https://glacial-falls-40496.herokuapp.com/api/Artwork/:" + searchTerm,
-        method: "GET"
-    }).then(function(response) {
-        $("#resultsHere").empty();
-        $("#resultsHere").append(JSON.stringify(response));
-    })
+    console.log(searchTerm);
+    // var firstWord = codeLine.substr(0, codeLine.indexOf(" "));
+    // console.log(firstWord);
+    // $.ajax({
+    //     url: "https://https://glacial-falls-40496.herokuapp.com/api/Artwork/:" + searchTerm,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     $("#resultsHere").empty();
+    //     $("#resultsHere").append(JSON.stringify(response));
+    // })
 });
