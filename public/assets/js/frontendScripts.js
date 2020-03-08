99e6fd7ce074bb708bd13520308e17a4b18462a0
Matthew Wester <MatthewWester@live.com>
2020-03-07 17:21:38 -0700

    Ajax call not a function even though using minified version jquery

--- public/assets/js/frontendScripts.js
+++ public/assets/js/frontendScripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $("#searchButton").click(function () {
         event.preventDefault();
-        console.log("searchArtwork function starts");
         var searchTerm = $("#searchArtwork").val().toLowerCase();
         console.log(searchTerm + " is the search term");
         $.ajax({
             url: "/api/Artwork/" + searchTerm,
             method: "GET"
         }).then(function (response) {
+            console.log(response);
             $("#resultsHere").empty();
             $("#resultsHere").append(JSON.stringify(response));
         })
