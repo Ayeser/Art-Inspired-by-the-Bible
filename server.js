81227e3b1bd1c3dc9528db1663c9a4e8274c1206
Matthew Wester <MatthewWester@live.com>
2020-03-08 09:15:36 -0700

    adjusted server file

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 // const routes = require("./controllers/bible_controller.js");
-const db = require("./models");
+const db = require("./models/");
 
 // app.use(routes);
 
@@ -27,7 +27,7 @@ require("./routes/bible_controller.js")(app);
 
 
 // Start our server so that it can begin listening to client requests.
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
