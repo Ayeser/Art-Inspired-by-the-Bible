60e4bf99c9f57c8ae90c19a850237150553275b8
Matthew Wester <MatthewWester@live.com>
2020-03-07 20:29:21 -0700

    did sequelize init then put in duplicate code as before

---
+++ models/scriptures.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes) {
+    const Scripture = sequelize.define("Scripture", {
+      book: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 140]
+        }
+      },
+      chapter: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          validate: {
+              min: 1,
+              max: 50,
+          }
+      },
+      passage: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    createdAt: {
+      type:DataTypes.DATE(3),
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)')
+    },
+    updatedAt: {
+      type:DataTypes.DATE(3),
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)')
+    }
+});
+    return Scripture;
+  };
