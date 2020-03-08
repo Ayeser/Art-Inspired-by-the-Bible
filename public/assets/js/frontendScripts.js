0687291c25595237b4fe6ff4be5e98f0571315e3
Matthew Wester <MatthewWester@live.com>
2020-03-07 18:24:46 -0700

    Ids added to html elements to now complete .get call

--- public/assets/js/frontendScripts.js
+++ public/assets/js/frontendScripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $(document).on("submit", "#artworkForm", handleArtworkFormSubmit);
-
+    var searchInput = $("#searchArtwork")
     function handleArtworkFormSubmit(event) {
         event.preventDefault();
         if (!searchInput.val().trim().trim()) {
             return;
         }
-        $.get("api/Artwork", function(data) {
+        $.get("api/Artwork/" + searchInput, function(response) {
             console.log(response);
             $("#resultsHere").empty();
             $("#resultsHere").append(JSON.stringify(response));
