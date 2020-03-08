56a187f451492694879b3ed98552c8fa4595ee4c
Matthew Wester <MatthewWester@live.com>
2020-03-07 17:15:08 -0700

    For now am using search input totally for ajax call

--- public/assets/js/frontendScripts.js
+++ public/assets/js/frontendScripts.js
@@ -3,11 +3,9 @@ $(document).ready(function () {
         event.preventDefault();
         console.log("searchArtwork function starts");
         var searchTerm = $("#searchArtwork").val().toLowerCase();
-        var firstWord = searchTerm.substr(0, searchTerm.indexOf(" "));
         console.log(searchTerm + " is the search term");
-        console.log("The first word of search is " + firstWord);
-        $.getJSON({
-            url: "/api/Artwork/" + this.searchTerm,
+        $.ajax({
+            url: "/api/Artwork/" + searchTerm,
             method: "GET"
         }).then(function (response) {
             $("#resultsHere").empty();
