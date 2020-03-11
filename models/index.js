24d9409adf919c9bcfaef38c6642ca0f54abd5a7
Matthew Wester <MatthewWester@live.com>
2020-03-10 23:31:20 -0700

    For now took out FileUploadWithPreview block of code

--- models/index.js
+++ models/index.js
@@ -65,20 +65,3 @@ verse:1
 }).then(function(dbUser) {
 console.log(dbUser);
 });
-
-//below chunk is for the image uploader on the contest page
-var upload = new FileUploadWithPreview('myUniqueUploadId', {
-  showDeleteButtonOnImages: true,
-  text: {
-      chooseFile: 'Custom Placeholder Copy',
-      browse: 'Custom Button Copy',
-      selectedCount: 'Custom Files Selected Copy',
-  },
-  images: {
-      baseImage: importedBaseImage,
-  },
-  presetFiles: [
-      '../public/logo-promosis.png',
-      'https://images.unsplash.com/photo-1557090495-fc9312e77b28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
-  ],
-});
