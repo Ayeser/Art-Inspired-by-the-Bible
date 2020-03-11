821bb6e576a144c3311c43ad3e95fee5cfbcd6f0
Matthew Wester <MatthewWester@live.com>
2020-03-10 22:49:18 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -19,29 +19,18 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 // Import routes and give the server access to them.
-<<<<<<< HEAD
-<<<<<<< HEAD
-const routes = require("./controllers/bible_controller.js");
+
 const db = require("./models/artpieces.js");
-=======
-// const routes = require("./controllers/bible_controller.js");
+
 const db = require("./models");
->>>>>>> 52a2154427c3d97332a0d1388f93fc2db59d4e89
-=======
-const db = require("./models/");
->>>>>>> d653f89a126fe83d6ac6d6fa51b3779aeaf2fea4
 
-// app.use(routes);
 
-// require("./routes/bible_controller.js")(app);
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
 
 // Start our server so that it can begin listening to client requests.
-<<<<<<< HEAD
-=======
 db.sequelize.sync().then(function() {
->>>>>>> d653f89a126fe83d6ac6d6fa51b3779aeaf2fea4
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+  })
+});
