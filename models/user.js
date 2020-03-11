712f92a389f683ee5f12dd74e0befe28e2f74ff4
Matthew Wester <MatthewWester@live.com>
2020-03-10 17:06:07 -0700

    typo fix in user.js model

--- models/user.js
+++ models/user.js
@@ -49,7 +49,7 @@ module.exports = function(sequelize, DataTypes) {
         type: DataTypes.BOOLEAN,
         default: false
     },
-    firstSamuel, Achievement: {
+    firstSamuelAchievement: {
         type: DataTypes.BOOLEAN,
         default: false
     },
