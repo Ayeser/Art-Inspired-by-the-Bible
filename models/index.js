365e2966e085bc8cbe88430621ae626f81359744
Matthew Wester <MatthewWester@live.com>
2020-03-10 22:58:41 -0700

    undoing the errors merging created

--- models/index.js
+++ models/index.js
@@ -10,42 +10,22 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  var sequelize = new sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-=======
   sequelize = new Sequelize(process.env[config.use_env_variable]);
-=======
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
->>>>>>> d653f89a126fe83d6ac6d6fa51b3779aeaf2fea4
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
->>>>>>> 52a2154427c3d97332a0d1388f93fc2db59d4e89
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
+  .filter(function(file) {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-<<<<<<< HEAD
   .forEach(function(file) {
-<<<<<<< HEAD
-    const model = sequelize["import"](path.join(__dirname, file));
-  })
-=======
-    let model = sequelize["import"](path.join(__dirname, file));
-=======
-  .forEach(file => {
     const model = sequelize['import'](path.join(__dirname, file));
->>>>>>> d653f89a126fe83d6ac6d6fa51b3779aeaf2fea4
     db[model.name] = model;
   });
->>>>>>> 52a2154427c3d97332a0d1388f93fc2db59d4e89
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
