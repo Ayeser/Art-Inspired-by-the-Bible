a1405dffd3c303c581cd7e05280ef62b904f88f2
Matthew Wester <MatthewWester@live.com>
2020-03-10 19:09:36 -0700

    adding html pages and html routes for login capability

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,4 +10,16 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/contest.html"));
       });
 
+      app.get("/login", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+      });
+
+      app.get("/profile", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/profile.html"));
+      });
+
+      app.get("/signup", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+      });
+
 }
