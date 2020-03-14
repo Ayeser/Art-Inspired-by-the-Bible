3bbd14c9f9f1ac9422b4521aa9bb9dd3ade93811
Matthew Wester <MatthewWester@live.com>
2020-03-13 20:01:12 -0700

    Not sure if last commit included frontend javascript update

--- public/assets/js/frontendScripts.js
+++ public/assets/js/frontendScripts.js
@@ -187,52 +187,7 @@ document.cookie = "cross-site-cookie=bar; SameSite=None; Secure";
         function createNewVideo(piece) {
             return('<div class="jumbotron jumbotron-fluid" style="width: 18rem;"><div class="card-body">' + piece.videoEmbed + '</div><div><h5 class="card-title">' + piece.title + '</h5></div><div>Artist: ' + piece.artist + '</div><div>Verse: ' + piece.verse + '</div></div>');
         }
-    }
-//This next section is for the contest page to submit a piece of art
-const loginForm = $("form.login");
-    const emailInput = $("input#email-input");
-    const passwordInput = $("input#password-input");
-  
-    // When the form is submitted, we validate there's an email and password entered
-    loginForm.on("submit", function(event) {
-      event.preventDefault();
-      const userData = {
-        email: emailInput.val().trim(),
-        password: passwordInput.val().trim()
-      };
-  
-      if (!userData.email || !userData.password) {
-        return;
-      }
-  
-      // If we have an email and password we run the loginUser function and clear the form
-      loginUser(userData.email, userData.password);
-      emailInput.val("");
-      passwordInput.val("");
-    });
-  
-    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-    function loginUser(email, password) {
-      $.post("/api/login", {
-        email: email,
-        deleteCode: password
-      })
-        .then(function(data) {
-          console.log(data.email + " " + data.password)
-          window.location.replace("/members");
-          // If there's an error, log the error
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-    }
-
-    $("#pictureSubmit").on("click", function () {
-        $.post("api/contestPiece/", { title: "Exodus", chapter: "1", picture: upload.cachedFileArray[0] })
-            .done(function (data) {
-                console.log("Picture uploaded: " + data);
-            });
-    });
+    };
 
     $(document).on("submit", "#artworkForm", searchBarSubmit);
     var searchTerms;
