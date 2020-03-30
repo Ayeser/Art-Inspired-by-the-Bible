e33e3de02684c1ef656e4828ca62e4824b176952
Matthew Wester <MatthewWester@live.com>
2020-03-29 17:15:06 -0700

    changed empty object in api call to req instead

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -96,7 +96,7 @@ module.exports = function (app) {
   });
 
   app.post("/api/signup", function (req, res) {
-    db.User.create({}).then(function () {
+    db.User.create(req).then(function () {
       res.send("User is created!");
   });
 });
