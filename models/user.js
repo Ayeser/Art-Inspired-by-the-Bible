f87591aa2fdb8a70568d318e28f6cdc38d334f5d
Matthew Wester <MatthewWester@live.com>
2020-03-29 19:36:26 -0700

    changing LONGTEXT to TEXT as datatype

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     tempLiked: {
-      type: DataTypes.LONGTEXT
+      type: DataTypes.TEXT
     },
     createdAt: {
       type:DataTypes.DATE(3),
